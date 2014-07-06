7f49d06428e29d4a804c17e92785f934a1f222d3
ThoughtWorker <foo@foo.com>
2014-07-06 12:23:37 -0500

    test

--- app/controllers/youth_hacker_controller_controller.rb
+++ app/controllers/youth_hacker_controller_controller.rb
@@ -7,7 +7,7 @@ class YouthHackerControllerController < ApplicationController
   def create 
     puts params
     youth = YouthHacker.create(params.require(:youth_hacker).permit(:age, :gender, :address, :category, :name, :tech))
-    all_hackers_matching = Mentor.where("category = #{youth.category} OR tech = #{tech}") 
+    all_hackers_matching = Mentor.where("category = #{youth.category}") 
     all_hackers_matching.each  do |hacker|
         if(hacker.address != nil && hacker.address != "")
           email_something(hacker)
