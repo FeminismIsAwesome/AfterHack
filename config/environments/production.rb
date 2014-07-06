adbed14709f3194cca44e63791fd7875e1c22f20
ThoughtWorker <foo@foo.com>
2014-07-06 14:11:09 -0500

    trying gmail instead of sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,16 +71,16 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-    config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
-  }
+ ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,  #this is the important shit!
+  :address => 'smtp.gmail.com', #'localhost', 
+  :port => 587,
+  :tls => true,
+  :domain => 'mail.google.com',  # mail.customdomain.com if you use google apps
+  :authentication => :login,
+  :user_name => ENV["GOOGLE_EMAIL"],
+  :password => ENV["GOOGLE_PASSWORD"]
+} 
     config.action_mailer.asset_host = "http://afterhack.herokuapp.com/"
 
 
